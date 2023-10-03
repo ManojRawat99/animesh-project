60c04e15265e27fdc69af27847dbee6544fe63e2
manoj <manojrawat.artzen@gmail.com>
2023-10-03 11:05:34 +0530

    new-design

---
+++ animesh-design/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      starting new design of react 
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
